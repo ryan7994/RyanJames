d55119885eae15b9438cda771db1d20be451f0ed
ryan7994 <ryanjames7994@gmail.com>
2015-10-06 02:10:25 +0800

    first commit

---
+++ Documents/Java/rjsm/ExceptionHandling/src/Finally.java
@@ -0,0 +1,42 @@
+
+public class Finally {
+
+	
+	public static void main (String[] args) {
+		
+		try {
+			throwException();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+	
+	public static void throwException() throws Exception {
+		try {
+			System.out.println("throwException()");
+			throw new Exception();
+		}
+		
+		finally {
+			System.err.println("finally: throwException");
+		}
+		
+		
+	}
+	
+	
+	public static void doesNotThrowException() {
+		try {
+			System.out.println("doesnotthrowException()");
+		}
+		catch(Exception e) {
+			System.out.println("Exception handled: doesnotthrowExcpetion");
+		}
+		finally {
+			System.err.println("finally: doesnotthrowException");
+		}
+		
+		
+	}
+}
